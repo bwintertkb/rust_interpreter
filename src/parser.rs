83f8e4d5082fed42a9d6387854029d6c301a0911
Bartek Winter <bartekwinter3@gmail.com>
2023-06-13 23:14:51 +0100

    Page 73, Integer Literals

--- src/parser.rs
+++ src/parser.rs
@@ -85,6 +85,7 @@ impl Parser {
     }
 
     pub fn next_token(&mut self) {
+        // Some comment
         self.curr_token = self.peek_token.clone();
         self.peek_token = Some(self.lexer.next_token());
     }
